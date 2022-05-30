4fcef5f20f48e51eeb26d91b44bc08049e270f4e
jeferson.silva <jeferson.silva@splab.ufcg.edu.br>
2022-05-30 14:25:37 -0300

    adding some spaces, nothing changes

--- src/main/java/com/api/salesreport/salesReport/controllers/ProductController.java
+++ src/main/java/com/api/salesreport/salesReport/controllers/ProductController.java
@@ -18,56 +18,52 @@ import com.api.salesreport.salesReport.entities.Product;
 import com.api.salesreport.salesReport.services.ProductService;
 
 @RestController
-@RequestMapping(value="/products")
+@RequestMapping(value = "/products")
 public class ProductController {
 
-    @Autowired
-    private ProductService productService;
-    
-    @RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private ProductService productService;
+
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Product>> findAll() {
 		List<Product> list = productService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
-    
-    @RequestMapping(value="/{id}", method= RequestMethod.GET)
-    public ResponseEntity<Product> findById(@PathVariable Integer id) {
-        Product obj = productService.findById(id);
-        return ResponseEntity.ok().body(obj);
-    }
-    
-    
-    @RequestMapping(value="/count", method= RequestMethod.GET)
-    public ResponseEntity<Map<String, Object>> countObj() {
-    	Integer count = productService.findAll().size();
-    	
-    	Map<String, Object> payload = new HashMap<>();
-        payload.put("count", count);
-        
-    	return ResponseEntity.ok().body(payload);
-    }
-    
-    
-    @RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Product> findById(@PathVariable Integer id) {
+		Product obj = productService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
+	public ResponseEntity<Map<String, Object>> countObj() {
+		Integer count = productService.findAll().size();
+
+		Map<String, Object> payload = new HashMap<>();
+		payload.put("count", count);
+
+		return ResponseEntity.ok().body(payload);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Product obj) {
 		obj = productService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-    
-    
-    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Product obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = productService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-    
-    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-    	productService.delete(id);
+		productService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
