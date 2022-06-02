94baf2895f33afceb2aa4bb5501d90886522cbc1
Marcos Guillermo <marcosgdn@salesForce-1116.localdomain>
2022-06-02 14:00:02 -0300

    (broken) enabling cors

--- src/main/java/com/api/salesreport/salesReport/SalesReportApplication.java
+++ src/main/java/com/api/salesreport/salesReport/SalesReportApplication.java
@@ -2,11 +2,13 @@ package com.api.salesreport.salesReport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebMvc
 public class SalesReportApplication {
 
 	public static void main(String[] args) {
